39b71517dc26568841764b28f7f337801937ca39
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2023-01-12 20:05:37 -0600

    Finish Stabilizer Tooltip

--- java/tb/common/event/TBEventHandler.java
+++ java/tb/common/event/TBEventHandler.java
@@ -2,12 +2,17 @@
  
  import java.lang.reflect.Method;
 
+import com.google.common.eventbus.EventBus;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
 import net.minecraft.init.Items;
+import net.minecraft.launchwrapper.Launch;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import tb.asm.TBCoreTransformer;
+import tb.core.TBCore;
 import thaumcraft.api.crafting.IInfusionStabiliser;
  
  
@@ -21,20 +26,27 @@ import thaumcraft.api.crafting.IInfusionStabiliser;
    }
    
   @SubscribeEvent
-  public static void makeToolTip(ItemTooltipEvent event) {
+  public void makeToolTip(ItemTooltipEvent event) {
 	  if(event.itemStack.getItem().equals(Items.skull))
-		  event.toolTip.add(StatCollector.translateToLocal("hello from tb asm"));
+		  event.toolTip.add(StatCollector.translateToLocal("Can be used as an infusion stabilizer"));
 	  else if(Block.getBlockFromItem(event.itemStack.getItem())!=null)
 		  for(Method m : Block.getBlockFromItem(event.itemStack.getItem()).getClass().getMethods())
 			  if(m.getName().endsWith("canStabaliseInfusion")) {
 
-				  event.toolTip.add(StatCollector.translateToLocal("has method"));
+				  //event.toolTip.add(StatCollector.translateToLocal("has method"));
 				  Block sBlock = Block.getBlockFromItem(event.itemStack.getItem());
 					
 				  IInfusionStabiliser conv = (IInfusionStabiliser) sBlock;
-					
-					
-				  event.toolTip.add(StatCollector.translateToLocal("Can stabalize: " + String.valueOf(conv.canStabaliseInfusion(event.entity.worldObj, 0, 0, 0))));
+				  if (conv.canStabaliseInfusion(event.entity.worldObj, 0, 0, 0)) {
+					  event.toolTip.add(StatCollector.translateToLocal("Can be used as an infusion stabilizer"));
+				  }
+				  
+				  if (TBCore.isDev) {
+					  boolean isDeob = (Boolean)Launch.blackboard.get("fml.deobfuscatedEnvironment");
+					 event.toolTip.add(StatCollector.translateToLocal("Is deob: " + isDeob));
+					 event.toolTip.add(StatCollector.translateToLocal("Got into patching method: " + TBCoreTransformer.inPatchingMethod));
+					 event.toolTip.add(StatCollector.translateToLocal("Got into tooltip method: " + TBCoreTransformer.inTooltipMethod));
+				  }
 			  }			
 	}
  }
