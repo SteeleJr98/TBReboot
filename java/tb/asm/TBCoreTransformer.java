51b65d888601fee1ff88e9ea91cb2d7e2056639b
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2023-01-12 20:02:02 -0600

    Finish WG asm

--- java/tb/asm/TBCoreTransformer.java
+++ java/tb/asm/TBCoreTransformer.java
@@ -1,8 +1,11 @@
 package tb.asm;
 
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.logging.log4j.Level;
@@ -27,17 +30,22 @@ import net.minecraft.init.Items;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.NextTickListEntry;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.steelehook.SteeleCore.Handlers.Logging;
 import scala.collection.parallel.ParIterableLike.Forall;
 import scala.reflect.internal.Trees.New;
 import tb.common.block.TBBlock;
 import tb.core.TBCore;
 import thaumcraft.api.crafting.IInfusionStabiliser;
+import thaumcraft.common.Thaumcraft;
 
 public class TBCoreTransformer implements IClassTransformer {
 
 	static boolean isDeob;
+	private static int[] WGBytePattern = {Opcodes.NEW};
+	public static boolean inPatchingMethod = false;
+	public static boolean inTooltipMethod = false;
+
 	
 	@Override
 	public byte[] transform(String className, String newClassName, byte[] origCode) {
@@ -123,52 +131,153 @@ public class TBCoreTransformer implements IClassTransformer {
 	
 	
 	
-	private byte[] patchToolTips(byte[] origCode, boolean deobf)
-	{
-		TBCore.TBLogger.log(Level.INFO, "[CORE] Patching test nothing here");
-
-		final String methodToPatch = "getTooltip";
-		final String methodToPatch_srg = "func_70670_a";
-		final String methodToPatch_obf = "a";
-		final String desc1 = "(Lnet/minecraftforge/event/entity/player/ItemTooltipEvent;)V";
-		final String desc1_obf = "(Lrw;)V";
-		final String desc2 = deobf?"(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/potion/PotionEffect;)V":"(Lsv;Lrw;)V";
-
-		ClassReader cr = new ClassReader(origCode);
-
-		ClassNode classNode=new ClassNode();
-		cr.accept(classNode, 0);
-		for(MethodNode methodNode : classNode.methods)
-			if( (methodNode.name.equals(methodToPatch)||methodNode.name.equals(methodToPatch_srg)||methodNode.name.equals(methodToPatch_obf)) && (methodNode.desc.equals(desc1)||methodNode.desc.equals(desc1_obf)))
-			{
-				Iterator<AbstractInsnNode> insnNodes=methodNode.instructions.iterator();
-				while(insnNodes.hasNext())
-				{
-					AbstractInsnNode insn=insnNodes.next();
-					if(insn.getOpcode()==Opcodes.IRETURN
-							||insn.getOpcode()==Opcodes.RETURN
-							||insn.getOpcode()==Opcodes.ARETURN
-							||insn.getOpcode()==Opcodes.LRETURN
-							||insn.getOpcode()==Opcodes.DRETURN)
-					{
-						InsnList endList=new InsnList();
-						//endList.add(new VarInsnNode(Opcodes.ALOAD, 0));
-						endList.add(new VarInsnNode(Opcodes.ALOAD, 1));
-						endList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "tb/asm/TBCoreTransformer", "makeToolTip", desc1, false));
-						methodNode.instructions.insertBefore(insn, endList);
+//	private byte[] patchToolTips(byte[] origCode, boolean deobf)
+//	{
+//		TBCore.TBLogger.log(Level.INFO, "[CORE] Patching test nothing here");
+//
+//		final String methodToPatch = "getTooltip";
+//		final String methodToPatch_srg = "func_70670_a";
+//		final String methodToPatch_obf = "a";
+//		final String desc1 = "(Lnet/minecraftforge/event/entity/player/ItemTooltipEvent;)V";
+//		final String desc1_obf = "(Lrw;)V";
+//		final String desc2 = deobf?"(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/potion/PotionEffect;)V":"(Lsv;Lrw;)V";
+//
+//		ClassReader cr = new ClassReader(origCode);
+//
+//		ClassNode classNode=new ClassNode();
+//		cr.accept(classNode, 0);
+//		for(MethodNode methodNode : classNode.methods)
+//			if( (methodNode.name.equals(methodToPatch)||methodNode.name.equals(methodToPatch_srg)||methodNode.name.equals(methodToPatch_obf)) && (methodNode.desc.equals(desc1)||methodNode.desc.equals(desc1_obf)))
+//			{
+//				Iterator<AbstractInsnNode> insnNodes=methodNode.instructions.iterator();
+//				while(insnNodes.hasNext())
+//				{
+//					AbstractInsnNode insn=insnNodes.next();
+//					if(insn.getOpcode()==Opcodes.ALOAD) 
+//					{
+//						InsnList endList=new InsnList();
+//						//endList.add(new VarInsnNode(Opcodes.ALOAD, 0));
+//						endList.add(new VarInsnNode(Opcodes.ALOAD, 1));
+//						endList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "tb/asm/TBCoreTransformer", "makeToolTip", desc1, false));
+//						methodNode.instructions.insertBefore(insn, endList);
+//
+//					}
+//				}
+//			}
+//		ClassWriter cw=new ClassWriter(ClassWriter.COMPUTE_MAXS);
+//		classNode.accept(cw);
+//
+//		return cw.toByteArray();
+//	}
+	
+	
+	private byte[] patchToolTips(byte[] origCode, boolean isDeob) {
+		
+		System.out.println("+++++++++In patching method+++++++++");
+		inPatchingMethod = true;
+		
+		ClassNode classNode = new ClassNode();
+		ClassReader classReader = new ClassReader(origCode);
+		classReader.accept(classNode, 0);
+		
+		
+		int interfaceCounter = 0;
+		List<Integer> interfaceIndexList = new ArrayList<Integer>();
+		
+		Iterator<MethodNode> methods = classNode.methods.iterator();
+		while(methods.hasNext()) {
+			MethodNode m = methods.next();
+			
+			if (m.name.equals("getTooltip")) {
+				System.out.println("+++++++++Found WG tooltip Method+++++++++");
+				inTooltipMethod = true;
+				AbstractInsnNode currentNode = null;
+				AbstractInsnNode targetNode = null;
+				int popIndex = -1;
+				Iterator<AbstractInsnNode> iter = m.instructions.iterator();
+				int index = -1;
+				while (iter.hasNext()) {
+					index++;
+					currentNode = iter.next();
+					
+					if (currentNode.getOpcode() == Opcodes.INVOKEINTERFACE) { //need interface 2 & 3
+						targetNode = currentNode;
+						popIndex = index;
+						interfaceCounter++;
+						interfaceIndexList.add(index);
+						//System.out.println("-----------------Have pop index-----------------");
+						//System.out.println(targetNode.getOpcode());
+						//System.out.println(popIndex);
+						//break;
 					}
 				}
+				
+				
+				System.out.println("INVOKEINTERFACE count: " + interfaceCounter);
+				//interfaceIndexList.remove(0);
+				System.out.println("List Size: " + interfaceIndexList.size());
+				
+				for (int i = 0; i < interfaceIndexList.size(); i++) {
+					System.out.println(interfaceIndexList.get(i));
+				}
+//				
+//				System.out.println("New List Size: " + interfaceIndexList.size());
+				
+//				List<AbstractInsnNode> remNodes = new ArrayList<AbstractInsnNode>();
+//				for (int i = 0; i < 10; i++) {
+//					//remNodes.add(m.instructions.get(i + 1));
+//					//System.out.println(i + popIndex + 1);
+//					//System.out.println(m.instructions.get(i + popIndex).getOpcode());
+//					//m.instructions.remove(m.instructions.get(i + popIndex));
+//				}
+				
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1)));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) + 1));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 1));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 2));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 3));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 4));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 5));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 6));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 7));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 8));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 9));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) - 10));
+				
+				//35
+				for (int i = 1; i >= -35; i--) {
+					m.instructions.remove(m.instructions.get(interfaceIndexList.get(2) + i)); //remove WG tooltip for blocks
+				}
+				
+				for (int i = 1; i >= -10; i--) {
+					m.instructions.remove(m.instructions.get(interfaceIndexList.get(1) + i)); //remove EG tooltip for skulls
+				}
+				
+				
+				
+				
+				
+				
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(2)));
+//				m.instructions.remove(m.instructions.get(interfaceIndexList.get(2) + 1));
+				
+				
+				
+				
+				break;
 			}
-		ClassWriter cw=new ClassWriter(ClassWriter.COMPUTE_MAXS);
-		classNode.accept(cw);
-
-		return cw.toByteArray();
+		}
+		
+		ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+		classNode.accept(classWriter);
+		
+		return classWriter.toByteArray();
+		
 	}
 	
 	
 	
-	
-	public void TestMethod(ItemTooltipEvent event) {
+	public static void makeToolTip(ItemTooltipEvent event) {
 		TBCore.TBLogger.log(Level.INFO, "THIS IS A TEST MESSAGE");
 	}
 	
