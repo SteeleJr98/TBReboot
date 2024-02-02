5cd36a6a80fa5397af475be92a757581ed9650a0
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2024-02-02 16:01:14 -0600

    Added creative tabs

---
+++ java/tb/core/CreativeTabBlocks.java
@@ -0,0 +1,17 @@
+package tb.core;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import tb.init.TBBlocks;
+
+public class CreativeTabBlocks {
+	public static final CreativeTabs tbTabBlocks = new CreativeTabs(TBCore.modid.toLowerCase() + ".blocks.name") {
+		
+		@Override
+		public Item getTabIconItem() {
+			// TODO Auto-generated method stub
+			return Item.getItemFromBlock(TBBlocks.voidAnvil);
+		}
+	};
+
+}
---
+++ java/tb/core/CreativeTabItems.java
@@ -0,0 +1,17 @@
+package tb.core;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import tb.init.TBItems;
+
+public class CreativeTabItems {
+	public static final CreativeTabs tbTabItems = new CreativeTabs(TBCore.modid.toLowerCase() + ".items.name") {
+		
+		@Override
+		public Item getTabIconItem() {
+			// TODO Auto-generated method stub
+			return TBItems.tobaccoSeeds;
+		}
+	};
+	
+}
