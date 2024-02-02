26494342f99638561f08cebdb588d11abb8972b9
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2024-02-02 15:43:45 -0600

    Changed Tobacco bool to int

--- java/tb/api/ITobacco.java
+++ java/tb/api/ITobacco.java
@@ -3,7 +3,7 @@ package tb.api;
 import net.minecraft.entity.player.EntityPlayer;
 
 public interface ITobacco {
-  void performTobaccoEffect(EntityPlayer paramEntityPlayer, int paramInt, boolean paramBoolean);
+  void performTobaccoEffect(EntityPlayer paramEntityPlayer, int paramInt, int paramBoolean);
 }
 
 
