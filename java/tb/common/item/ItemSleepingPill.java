74bce67bcfe3c51cc641be5668bb1154ef598370
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2024-02-02 15:57:29 -0600

    Test Item

---
+++ java/tb/common/item/ItemSleepingPill.java
@@ -0,0 +1,28 @@
+package tb.common.item;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.S0APacketUseBed;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.World;
+
+public class ItemSleepingPill extends Item {
+	
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+
+		if (!world.isRemote) {
+			
+			//player.setPosition((double)((float)p_71018_1_ + 0.5F), (double)((float)p_71018_2_ + 0.9375F), (double)((float)p_71018_3_ + 0.5F));
+			
+		}
+        return stack;
+    }
+}
